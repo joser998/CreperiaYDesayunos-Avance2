ccbbe785d8c12209cd071265093b63cacdabaaed
joser998 <joseluisramirezambriz@gmail.com>
2020-09-09 23:51:00 -0500

    Segundo avance

---
+++ src/modelo/ConsultasProducto.java
@@ -0,0 +1,307 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+public class ConsultasProducto extends Conexion{
+	PreparedStatement ps; 
+    ResultSet rs; 
+    
+    
+    
+    /*  Tabla Categoria */
+    public boolean insertarCategoria(ProductoCategoria producto) {
+    	Connection conexion = getConnection();
+    	
+    	try{
+            ps = conexion.prepareStatement("insert into categoria (categoria, codigo, descripcion) values (?,?,?)");
+            ps.setString(1, producto.getCategoria());
+            ps.setString(2, producto.getCodigoTablaCategoria());
+            ps.setNString(3, producto.getDescripcion());
+            
+            int resultado = ps.executeUpdate(); 
+            
+           
+            if(resultado > 0){
+                return true;
+            }else{
+                return false;
+            }
+            
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "Error: "+ex);
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, "Error al cerrar Conexion: "+ex);
+            }
+        }
+    	
+    } // *************************************************
+    
+    
+    public boolean buscarCategoriaPorId(ProductoCategoria producto) {
+    	Connection conexion = getConnection();
+    	
+    	try{
+            ps = conexion.prepareStatement("select * from categoria where idCategoria=?");
+            ps.setInt(1, producto.getIdCategoria());
+            rs = ps.executeQuery(); 
+            
+            
+            if(rs.next() == true){
+            	producto.setIdCategoria(rs.getInt("idCategoria"));
+            	producto.setCategoria(rs.getString("categoria"));
+            	producto.setCodigo(rs.getString("codigo"));
+            	producto.setDescripcion(rs.getString("descripcion"));
+            	
+                return true;
+                
+            }else{
+                return false;
+            }
+            
+        }catch(Exception ex){
+            
+            JOptionPane.showMessageDialog(null, "Error: "+ex);
+            
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, "Error al cerrar Conexion: "+ex);
+            }
+        }
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public boolean buscarCategoria(ProductoCategoria producto) {
+    	
+    	Connection conexion = getConnection();
+    	
+    	
+    	try{
+            ps = conexion.prepareStatement("select * from categoria where codigo=?");
+            ps.setString(1, producto.getCodigoTablaCategoria());
+            rs = ps.executeQuery(); 
+            
+            
+            if(rs.next() == true){
+            	producto.setIdCategoria(rs.getInt("idCategoria"));
+            	producto.setCategoria(rs.getString("categoria"));
+            	producto.setCodigo(rs.getString("codigo"));
+            	producto.setDescripcion(rs.getString("descripcion"));
+            	
+                return true;
+                
+            }else{
+                return false;
+            }
+            
+        }catch(Exception ex){
+            
+            JOptionPane.showMessageDialog(null, "Error: "+ex);
+            
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, "Error al cerrar Conexion: "+ex);
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public boolean insertar(ProductoCategoria producto){
+        Connection conexion = getConnection();
+    
+    try{
+        ps = conexion.prepareStatement("insert into producto (platillo, codigo, precio, cantidad, ingredientes, codigoCategoria) values (?,?,?,?,?,?)");
+        ps.setString(1, producto.getPlatillo());
+        ps.setString(2, producto.getCodigo());
+        ps.setString(3, producto.getPrecio());
+        ps.setString(4, producto.getCantidad());
+        ps.setString(5, producto.getIngredientes());
+        ps.setInt(6, producto.getCodigoCategoria());
+        
+       
+        int resultado = ps.executeUpdate(); 
+        
+       
+        if(resultado > 0){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }catch(Exception ex){
+        JOptionPane.showMessageDialog(null, "Error: "+ex);
+        return false;
+    }finally{
+        try{
+            conexion.close();
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "Error al cerrar Conexion: "+ex);
+        }
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    //********
+    
+    public boolean buscar(ProductoCategoria producto){
+       
+        Connection conexion = getConnection(); 
+        
+        
+        try{
+            ps = conexion.prepareStatement("select * from producto where codigo=?");
+            ps.setString(1, producto.getCodigo());
+            rs = ps.executeQuery(); 
+            
+            
+            if(rs.next() == true){
+            	producto.setIdProducto(rs.getInt("idProducto"));
+            	producto.setPlatillo(rs.getString("platillo"));
+            	producto.setCodigo(rs.getString("codigo"));
+            	producto.setPrecio(rs.getString("precio"));
+            	producto.setCantidad(rs.getString("cantidad"));
+            	producto.setIngredientes(rs.getString("ingredientes"));
+            	producto.setCodigoCategoria(Integer.parseInt(rs.getString("codigoCategoria")));
+                  
+              
+                return true;
+                
+            }else{
+                return false;
+            }
+            
+        }catch(Exception ex){
+            
+            JOptionPane.showMessageDialog(null, "Error: "+ex);
+            
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, "Error al cerrar Conexion: "+ex);
+            }
+        }
+    }
+    
+    
+    
+    public boolean modificar(ProductoCategoria producto){
+       
+        Connection conexion = getConnection(); 
+       
+        try{
+            ps = conexion.prepareStatement("update producto set platillo=?, codigo=?, precio=?, cantidad=?, ingredientes=?, codigoCategoria=? where idProducto=?");
+            ps.setString(1, producto.getPlatillo());
+            ps.setString(2, producto.getCodigo());
+            ps.setString(3, producto.getPrecio());
+            ps.setString(4, producto.getCantidad());
+            ps.setString(5, producto.getIngredientes());
+            ps.setInt(6, producto.getCodigoCategoria());
+            ps.setInt(7, producto.getIdProducto());
+            
+                    
+            int resultado = ps.executeUpdate(); 
+            
+            
+            if(resultado > 0){
+                return true;
+            }else{
+                return false;
+            }
+            
+        }catch(Exception ex){
+            
+            JOptionPane.showMessageDialog(null, "Error: "+ex);
+            
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, "Error al cerrar Conexion: "+ex);
+            }
+        }
+    }
+    
+    
+    
+    public boolean eliminar(ProductoCategoria producto){
+       
+        Connection conexion = getConnection(); 
+        
+        
+        try{
+            
+            ps = conexion.prepareStatement("delete from producto where idProducto=?");
+            ps.setInt(1, producto.getIdProducto());
+                    
+            int resultado = ps.executeUpdate(); 
+            
+            
+            if(resultado > 0){
+                return true;
+            }else{
+                return false;
+            }
+            
+        }catch(Exception ex){
+            
+            JOptionPane.showMessageDialog(null, "Error: "+ex);
+            
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null, "Error al cerrar Conexion: "+ex);
+            }
+        }
+    }
+    
+}
---
+++ src/modelo/ProductoCategoria.java
@@ -0,0 +1,101 @@
+package modelo;
+
+public class ProductoCategoria {
+	/* Atributos Tabla Producto */
+	private int idProducto;
+	private String platillo;
+	private String codigo;
+	private String precio;
+	private String cantidad;
+	private String ingredientes;
+	private int codigoCategoria;
+	/* ************************* */
+	
+	
+	
+	/* Getters and Setters Tabla Producto */
+	public int getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+	public String getPlatillo() {
+		return platillo;
+	}
+	public void setPlatillo(String platillo) {
+		this.platillo = platillo;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	public String getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+	public String getIngredientes() {
+		return ingredientes;
+	}
+	public void setIngredientes(String ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+	public int getCodigoCategoria() {
+		return codigoCategoria;
+	}
+	public void setCodigoCategoria(int codigoCategoria) {
+		this.codigoCategoria = codigoCategoria;
+	}
+	/* ****************************************************** */
+	
+	
+	
+	
+	
+	/* Atributos Tabla Categoria */
+	private int idCategoria;
+	private String categoria;
+	private String descripcion;
+	private String codigoTablaCategoria;
+	/* ************************* */
+	
+	
+	/* Getters and Setters Tabla Categoria */
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getCodigoTablaCategoria() {
+		return codigoTablaCategoria;
+	}
+	public void setCodigoTablaCategoria(String codigoTablaCategoria) {
+		this.codigoTablaCategoria = codigoTablaCategoria;
+	}
+	
+	/* ******************************************* */
+	
+}
